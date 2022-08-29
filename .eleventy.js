a61371a9b1a8265a197f59c2bb092aca1f32e9f1
Stefan Allchorne <allchorne39@sky.com>
2022-08-29 21:12:52 +0100

    Open browser when running scripts

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,13 @@ const { DateTime } = require("luxon")
 
 module.exports = function(eleventyConfig) {
 
+    //from 11tyrocks: https://11ty.rocks/eleventyjs/dates/
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+    })
+
+    eleventyConfig.setBrowserSyncConfig({
+        open: true
     })
 
     return {
