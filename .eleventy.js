2c0219e91e64e22bc155c36b11bad69462441bd9
Stefan Allchorne <allchorne39@sky.com>
2022-08-30 22:33:41 +0100

    Organise the css into sass form

--- .eleventy.js
+++ .eleventy.js
@@ -24,6 +24,6 @@ module.exports = function(eleventyConfig) {
             input: "src",
             output: "public"
         },
-        passthroughFileCopy: true,
+        // passthroughFileCopy: true,
     }
 }
