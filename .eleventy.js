40d5512a98fd533b3aadf310badb7d1f641f19ff
Stefan Allchorne <allchorne39@sky.com>
2022-08-30 09:44:34 +0100

    Install autoprefixer, watch sass in 11ty and add relative css path in <head>
    Only installed autoprefixer, did not config or add it to package.json.

--- .eleventy.js
+++ .eleventy.js
@@ -2,11 +2,15 @@ const { DateTime } = require("luxon")
 
 module.exports = function(eleventyConfig) {
 
+    // Recomplile 11ty when files change
+    eleventyConfig.addWatchTarget("./src/style/")
+
     //from 11tyrocks: https://11ty.rocks/eleventyjs/dates/
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
     })
 
+    // Automatically open up the browser on script runs
     eleventyConfig.setBrowserSyncConfig({
         open: true
     })
